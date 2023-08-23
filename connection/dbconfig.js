a20266c0c00cef316fa57be55ed7da8052aa0183
saintnH <saint@nuvahub.com>
2023-08-22 18:57:17 -0600

    cambios para rama dev

--- connection/dbconfig.js
+++ connection/dbconfig.js
@@ -1,8 +1,8 @@
 const dbconfig = {
-  user: "*********",
-  password: "*********",
-  database: "*********",
-  server: "*********",
+  user: "nuvahubadmin",
+  password: "Pastiseta2018",
+  database: "nuvahubdemo",
+  server: "nuvahubserver.database.windows.net",
 };
 
 module.exports = dbconfig;
