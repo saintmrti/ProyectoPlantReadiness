a4c6cf30450977c41ce886b2b7339301fc1f7d85
saintnH <saint@nuvahub.com>
2023-09-06 17:29:54 -0600

    cambios en la db connection

--- connection/dbconfig.js
+++ connection/dbconfig.js
@@ -1,8 +1,15 @@
+// const dbconfig = {
+//   user: "nuvahubadmin",
+//   password: "Pastiseta2018",
+//   database: "nuvahubdemo",
+//   server: "nuvahubserver.database.windows.net",
+// };
+
 const dbconfig = {
-  user: "nuvahubadmin",
-  password: "Pastiseta2018",
-  database: "nuvahubdemo",
-  server: "nuvahubserver.database.windows.net",
+  user: "u_vKIn40Dev",
+  password: "MXb_HYsAJkdT",
+  database: "vkazr-db-in40-scus-test01",
+  server: "viakableazr-sql-scus-dev-test.database.windows.net",
 };
 
 module.exports = dbconfig;
