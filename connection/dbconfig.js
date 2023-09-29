7187d3d48af6a41df7c2a03a16ba292b6175c6e4
saintnH <saint@nuvahub.com>
2023-09-28 20:21:33 -0600

    cambios oculta bd

--- connection/dbconfig.js
+++ connection/dbconfig.js
@@ -1,15 +1,8 @@
-// const dbconfig = {
-//   user: "nuvahubadmin",
-//   password: "Pastiseta2018",
-//   database: "nuvahubdemo",
-//   server: "nuvahubserver.database.windows.net",
-// };
-
 const dbconfig = {
-  user: "u_vKIn40Dev",
-  password: "MXb_HYsAJkdT",
-  database: "vkazr-db-in40-scus-test01",
-  server: "viakableazr-sql-scus-dev-test.database.windows.net",
+  user: "*********",
+  password: "*********",
+  database: "*********",
+  server: "*********",
 };
 
 module.exports = dbconfig;
